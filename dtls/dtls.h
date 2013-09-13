1615beec8ed42a275f719870710f68ce931f97b7
karlo <ktsoriano2@upd.edu.ph>
2013-09-13 23:52:04 +0800

    add dtls.h

---
+++ dtls/dtls.h
@@ -0,0 +1,16 @@
+#ifndef ns_ping_h
+#define ns_ping_h
+
+#include "agent.h"
+#include "tclcl.h"
+#include "packet.h"
+#include "address.h"
+#include "ip.h"
+#include "sctp.h"
+
+class DtlsAgent : public Agent {
+public:
+	DtlsAgent();
+	virtual int command(int argc, const char*const* argv);
+	virtual void recv(Packet*, Handler*);
+};
