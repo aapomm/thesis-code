b6e0a2c1e293cd88077f2f59f20bd2ee1497581a
conradolega <conradolega@gmail.com>
2013-09-15 12:15:30 +0800

    New SCTP script

--- scripts/sctp.tcl
+++ scripts/sctp.tcl
@@ -18,9 +18,9 @@ $ns duplex-link $n0 $n1 1Mb 10ms DropTail
 
 set e [new Agent/Encapsulator]
 set d [new Agent/Decapsulator]
-set sctp0 [new Agent/SCTP]
 set udp0 [new Agent/UDP]
 set udp1 [new Agent/UDP]
+set sctp0 [new Agent/SCTP]
 set sctp1 [new Agent/SCTP]
 
 $ns attach-agent $n0 $udp0
@@ -28,8 +28,8 @@ $ns attach-agent $n1 $udp1
 $sctp0 target $e
 $e set status_ 1
 $e target $udp0
-$udp0 target $udp1
 $udp1 target $d
+$udp0 target $udp1
 $d target $sctp1
 
 set cbr [new Application/Traffic/CBR]
@@ -37,8 +37,8 @@ $cbr set packetSize_ 500
 $cbr set interval_ 0.05
 $cbr attach-agent $sctp0
 
-puts "Done configuring..."
+
 $ns at 0.5 "$cbr start"
 $ns at 4.5 "$cbr stop"
-puts "Running simulator..."
+$ns at 5.0 "finish"
 $ns run
