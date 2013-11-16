ac74cf90f556b8e9a151f4ec019bff64e4327947
conradolega <conradolega@gmail.com>
2013-11-16 13:22:48 +0800

    Working on TFRC implementation

--- sctp/sctp-tfrc.cc
+++ sctp/sctp-tfrc.cc
@@ -20,10 +20,67 @@
 #define SCTP_TFRC_RTT_Q 0.9
 #define SCTP_TFRC_RTT_Q2 0.9
 
+class TfrcSctpAgent;
+
+class SCTPTFRCSendTimer : public TimerHandler {
+
+	protected:
+		TfrcSctpAgent *agent_;
+	public:
+		SCTPTFRCSendTimer(TfrcSctpAgent* agent);
+		virtual void expire(Event *e);
+};
+
+class SCTPTFRCNoFeedbackTimer : public TimerHandler {
+
+	protected:
+		TfrcSctpAgent *agent_;
+	public:
+		SCTPTFRCNoFeedbackTimer(TfrcSctpAgent* agent);
+		virtual void expire(Event *e);
+};
+
+SCTPTFRCSendTimer::SCTPTFRCSendTimer(TfrcSctpAgent* agent) : TimerHandler() {
+	agent_ = agent;
+};
+
+void SCTPTFRCSendTimer::expire(Event *e) {
+
+};
+
+SCTPTFRCNoFeedbackTimer::SCTPTFRCNoFeedbackTimer(TfrcSctpAgent* agent) : TimerHandler() {
+	agent_ = agent;
+};
+
+void SCTPTFRCNoFeedbackTimer::expire(Event *e) {
+
+};
+
 class TfrcSctpAgent : public virtual SctpAgent {
 
 	public:
 		TfrcSctpAgent();
+	private:
+		double s_x_; 
+		double s_x_inst_;
+		double s_x_recv_;
+		double s_r_sample_;
+		double s_rtt_;
+		double s_r_sqmean_;
+		double s_p_;
+		double s_initial_x_;
+		double s_initial_rto_;
+		double s_rtt_q_;
+		double s_rtt_q2_;
+		double s_t_ld_;
+		
+		SCTPTFRCSendTimer *s_timer_send_;
+		SCTPTFRCNoFeedbackTimer *s_timer_no_feedback_;
+
+		/* Sender state */
+		/* Calculated send rate */
+		/* Packet size */
+
 };
 
 static class TfrcSctpClass : public TclClass {
@@ -37,4 +94,33 @@ static class TfrcSctpClass : public TclClass {
 
 TfrcSctpAgent::TfrcSctpAgent() : SctpAgent() {
 
+	s_x_ = SCTP_TFRC_INIT_SEND_RATE;
+	s_x_inst_ = SCTP_TFRC_INIT_SEND_RATE;
+
+	s_r_sample_ = 0.0;
+	s_rtt_ = 0.0;
+	s_r_sqmean_ = 0.0;
+	s_p_ = 0.0;
+
+	s_t_ld_ = -1.0;
+
+	s_timer_send_ = new SCTPTFRCSendTimer(this);
+	s_timer_no_feedback_ = new SCTPTFRCNoFeedbackTimer(this);
+
+	// Creating the agent, we want to initialize the values and the timers.
+	// The initialization is described in RFC 3448, section 4,
+	// "Sender Initialization".
 }
+
+// Define send and receive methods for sender and receiver.
+// t_ipi and delta calculations.
+
+// Send:
+// The first time that TFRC something, it has no information about
+// the network yet, so
+
+// The important thing is that the SendTimer is rescheduled 
+// whenever the sending rate X changes, and whenever the Timer
+// times out, we call the sendmsg function.
+
+// And that's it!
