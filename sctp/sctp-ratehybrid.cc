65bc336b4e8aba24e63715e13ce3c800d01adad4
aomanaloto1 <aaronpomanaloto@gmail.com>
2013-11-23 13:12:17 +0800

    tfrc integration

--- sctp/sctp-ratehybrid.cc
+++ sctp/sctp-ratehybrid.cc
@@ -6,6 +6,7 @@ static const char rcsid[] =
 #include "ip.h"
 #include "sctp-ratehybrid.h"
 #include "flags.h"
+#include "formula.h"
 
 #ifdef DMALLOC
 #include "dmalloc.h"
@@ -41,7 +42,35 @@ SctpRateHybrid::SctpRateHybrid() : SctpAgent()
 	timer_send = new SendTimer(this, pktQ_);
 
 	timer_send->sched(snd_rate);
-  total = 0;
+ 	total = 0;
+
+ 	//test initialization
+	// seqno_=0;				
+	// rate_ = InitRate_;
+	// delta_ = 0;
+	oldrate_ = rate_;  
+	// rate_change_ = SLOW_START;
+	// UrgentFlag = 1;
+	rtt_=0;	 
+	sqrtrtt_=1;
+	rttcur_=1;
+	tzero_ = 0;
+	// last_change_=0;	
+	maxrate_ = 0; 
+	ss_maxrate_ = 0;
+	// ndatapack_=0;
+	// ndatabytes_ = 0;
+	true_loss_rate_ = 0;
+	// active_ = 1; 
+	round_id = 0;
+	// heavyrounds_ = 0;
+	t_srtt_ = int(srtt_init_/tcp_tick_) << T_SRTT_BITS;
+	t_rttvar_ = int(rttvar_init_/tcp_tick_) << T_RTTVAR_BITS;
+	t_rtxcur_ = rtxcur_init_;
+	rcvrate = 0 ;
+	// all_idle_ = 0;
+
+	first_pkt_rcvd = 0 ;
 }
 
 SctpRateHybrid::~SctpRateHybrid(){
@@ -329,8 +358,23 @@ void SctpRateHybrid::recv(Packet *opInPkt, Handler*){
      	eSendNewDataChunks = FALSE; // reset AFTER sent (o/w breaks dependencies)
     }
 
+    TFRC_update(opInPkt);
+
+	//FREE PACKET
+ 	delete hdr_sctp::access(opInPkt)->SctpTrace();
+ 	hdr_sctp::access(opInPkt)->SctpTrace() = NULL;
+ 	Packet::free(opInPkt);
+ 	opInPkt = NULL;
+ 	delete [] ucpOutData;
+}
+
+void SctpRateHybrid::TFRC_update(Packet *pkt){
+    /* 
+   		TFRC INTEGRATION
+    */
 	double now = Scheduler::instance().clock();
-	hdr_sctp *nck = hdr_sctp::access(opInPkt);
+	//Extract SCTP headers for TFRC calculation
+	hdr_sctp *nck = hdr_sctp::access(pkt);
 	//double ts = nck->timestamp_echo;
 	double ts = nck->timestamp_echo + nck->timestamp_offset;
 	double rate_since_last_report = nck->rate_since_last_report;
@@ -339,10 +383,104 @@ void SctpRateHybrid::recv(Packet *opInPkt, Handler*){
 	int losses = nck->losses;
 	true_loss_rate_ = nck->true_loss;
 
-	//FREE PACKET
- 	delete hdr_sctp::access(opInPkt)->SctpTrace();
- 	hdr_sctp::access(opInPkt)->SctpTrace() = NULL;
- 	Packet::free(opInPkt);
- 	opInPkt = NULL;
- 	delete [] ucpOutData;
+	round_id ++;
+
+	if (round_id > 1 && rate_since_last_report > 0) {
+		/* compute the max rate for slow-start as two times rcv rate */ 
+		ss_maxrate_ = 2*rate_since_last_report*size_;
+		if (conservative_) { 
+			if (losses >= 1) {
+				/* there was a loss in the most recent RTT */
+				if (debug_) printf("time: %5.2f losses: %d rate %5.2f\n", 
+				  now, losses, rate_since_last_report);
+				maxrate_ = rate_since_last_report*size_;
+			} else { 
+				/* there was no loss in the most recent RTT */
+				maxrate_ = scmult_*rate_since_last_report*size_;
+			}
+			// if (debug_) printf("time: %5.2f losses: %d rate %5.2f maxrate: %5.2f\n", now, losses, rate_since_last_report, maxrate_);
+		} else 
+			maxrate_ = 2*rate_since_last_report*size_;
+	} else {
+		ss_maxrate_ = 0;
+		maxrate_ = 0; 
+	}
+
+	/* update the round trip time */
+	update_rtt (ts, now);
+
+	rcvrate = p_to_b(flost, rtt_, tzero_, fsize_, bval_);
+	/* if we get no more feedback for some time, cut rate in half */
+	double t = 2*rtt_ ; 
+	if (t < 2*size_/rate_) 
+		t = 2*size_/rate_ ; 
+	NoFeedbacktimer_.resched(t);
+	
+	/* if we are in slow start and we just saw a loss */
+	/* then come out of slow start */
+	if (first_pkt_rcvd == 0) {
+		first_pkt_rcvd = 1 ; 
+		slowstart();
+		nextpkt();
+	}
+	else {
+		if (rate_change_ == SLOW_START) {
+			if (flost > 0) {
+				rate_change_ = OUT_OF_SLOW_START;
+				oldrate_ = rate_ = rcvrate;
+			}
+			else {
+				slowstart();
+				nextpkt();
+			}
+		}
+		else {
+			if (rcvrate>rate_) 
+				increase_rate(flost);
+			else 
+				decrease_rate ();		
+		}
+	}
+	bool printStatus_ = true;
+	if (printStatus_) {
+		printf("time: %5.2f rate: %5.2f\n", now, rate_);
+		double packetrate = rate_ * rtt_ / size_;
+		printf("time: %5.2f packetrate: %5.2f\n", now, packetrate);
+		double maxrate = maxrate_ * rtt_ / size_;
+		printf("time: %5.2f maxrate: %5.2f\n", now, maxrate);
+	}
+}
+
+void SctpRateHybrid::update_rtt(double tao, double now){
+
+	t_rtt_ = int((now-tao) /tcp_tick_ + 0.5);
+	if (t_rtt_==0) t_rtt_=1;
+	if (t_srtt_ != 0) {
+		register short rtt_delta;
+		rtt_delta = t_rtt_ - (t_srtt_ >> T_SRTT_BITS);    
+		if ((t_srtt_ += rtt_delta) <= 0)    
+			t_srtt_ = 1;
+		if (rtt_delta < 0)
+			rtt_delta = -rtt_delta;
+	  	rtt_delta -= (t_rttvar_ >> T_RTTVAR_BITS);
+	  	if ((t_rttvar_ += rtt_delta) <= 0)  
+			t_rttvar_ = 1;
+	} else {
+		t_srtt_ = t_rtt_ << T_SRTT_BITS;		
+		t_rttvar_ = t_rtt_ << (T_RTTVAR_BITS-1);	
+	}
+	t_rtxcur_ = (((t_rttvar_ << (rttvar_exp_ + (T_SRTT_BITS - T_RTTVAR_BITS))) + t_srtt_)  >> T_SRTT_BITS ) * tcp_tick_;
+	tzero_=t_rtxcur_;
+ 	if (tzero_ < minrto_) 
+  		tzero_ = minrto_;
+
+	/* fine grained RTT estimate for use in the equation */
+	if (rtt_ > 0) {
+		rtt_ = df_*rtt_ + ((1-df_)*(now - tao));
+		sqrtrtt_ = df_*sqrtrtt_ + ((1-df_)*sqrt(now - tao));
+	} else {
+		rtt_ = now - tao;
+		sqrtrtt_ = sqrt(now - tao);
+	}
+	rttcur_ = now - tao;
 }
