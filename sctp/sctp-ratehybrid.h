8b9a8b54cd999553c8cd42b4290286c92e12f70d
conradolega <conradolega@gmail.com>
2013-12-28 19:06:33 +0800

    Fix loss intervals and weights initialization

--- sctp/sctp-ratehybrid.h
+++ sctp/sctp-ratehybrid.h
@@ -61,8 +61,8 @@ protected:
   // virtual void recv(Packet *pkt, Handler*);
 	
 	// Variables for sender-side p computation
-	int lossIntervals [8] = {0, 0, 0, 0, 0, 0, 0, 0};
-	double weights [8] = {1.0, 1.0, 1.0, 1.0, 0.8, 0.6, 0.4, 0.2};
+	int lossIntervals [9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+	double weights [9] = {1.0, 1.0, 1.0, 1.0, 1.0, 0.8, 0.6, 0.4, 0.2};
 	int currentLossIntervalLength;
 	int currentLossIntervalIndex = -1;
 
