47e2e8a39b245daaceaf35eea91ab5795143fb5f
karlo <ktsoriano2@upd.edu.ph>
2013-12-31 19:39:08 +0800

    fix tcl

--- scripts/smoothness/smoothness-hybrid.tcl
+++ scripts/smoothness/smoothness-hybrid.tcl
@@ -11,59 +11,63 @@ proc finish {} {
 	$ns flush-trace
 	close $nf
 	close $nd
-	exit 0
-}
+	exit 0 }
 
 set bottleneck [$ns node]
-
 
 # create tcp nodes and agents
 for {set i 0} {$i < 8} {incr i} {
 	# source
-	set tcp-src-n($i) [$ns node]	
-	set tcp-src($i) [new Agent/TCP/Sack1]
-	$ns duplex-link $tcp-src-n($i) $bottleneck 50Mb 2ms DropTail
-	$ns attach-agent $tcp-src-n($i) $tcp-src($i)
+	set tcp_src_n($i) [$ns node]	
+	set tcp_src($i) [new Agent/TCP/Sack1]
+	set tcp_traf($i) [new Application/FTP]
+	$ns duplex-link $tcp_src_n($i) $bottleneck 50Mb 2ms DropTail
+	$ns attach-agent $tcp_src_n($i) $tcp_src($i)
+	$tcp_traf($i) attach-agent $tcp_src($i) 
 	# sink
-	set tcp-snk-n($i) [$ns node]
-	$ns duplex-link $bottleneck $tcp-snk-n($i) 5Mb 20ms DropTail
-	$ns queue-limit $bottleneck $tcp-snk-n($i) 300
-	set tcp-snk($i) [new Agent/TCPSink]
-	$ns attach-agent $tcp-snk-n($i) $tcp-snk($i)
+	set tcp_snk_n($i) [$ns node]
+	$ns duplex-link $bottleneck $tcp_snk_n($i) 5Mb 20ms DropTail
+	$ns queue-limit $bottleneck $tcp_snk_n($i) 300
+	set tcp_snk($i) [new Agent/TCPSink]
+	$ns attach-agent $tcp_snk_n($i) $tcp_snk($i)
 
-	$ns connect tcp-src($i) tcp-snk($i)
+	$ns connect $tcp_src($i) $tcp_snk($i)
 }
 
 # create 8 (hybrid/sctp) nodes
 for {set i 0} {$i < 8} {incr i} {
 	# source
-	set sctp-src-n($i) [$ns node]
-	set sctp-src($i) [new Agent/SCTP]
-	$ns duplex-link $sctp-src-n($i) $bottleneck 50Mb 2ms DropTail
-	$ns attach-agent $sctp-src-n($i) $sctp-src($i)
+	set sctp_src_n($i) [$ns node]
+	set sctp_src($i) [new Agent/SCTP]
+	set sctp_traf($i) [new Application/FTP]
+	$ns duplex-link $sctp_src_n($i) $bottleneck 50Mb 2ms DropTail
+	$ns attach-agent $sctp_src_n($i) $sctp_src($i)
+	$sctp_traf($i) attach-agent $sctp_src($i) 
 	# sink
-	set sctp-snk-n($i) [$ns node]
-	$ns duplex-link $bottleneck $sctp-snk-n($i)  5Mb 20ms DropTail
-	$ns queue-limit $bottleneck $sctp-snk-n($i) 300
-	set sctp-src($i) [new Agent/SCTP]
-	$ns attach-agent $sctp-src-n($i) $sctp-src($i)
+	set sctp_snk_n($i) [$ns node]
+	$ns duplex-link $bottleneck $sctp_snk_n($i)  5Mb 20ms DropTail
+	$ns queue-limit $bottleneck $sctp_snk_n($i) 300
+	set sctp_snk($i) [new Agent/SCTP]
+	$ns attach-agent $sctp_src_n($i) $sctp_src($i)
 
-	$ns connect sctp-src($i) sctp-snk($i)
+	$ns connect $sctp_src($i) $sctp_snk($i)
 }	
 
 # create 2 udp nodes
 for {set i 0} {$i < 2} {incr i} {
 	# source
-	set udp-src-n($i) [$ns node]
-	set udp-src($i) [new Agent/UDP]
-	$ns duplex-link $udp-src-n($i) $bottleneck 50Mb 2ms DropTail
-	$ns attach-agent $udp-src-n($i) $sctp-src($i)
+	set udp_src_n($i) [$ns node]
+	set udp_src($i) [new Agent/UDP]
+	set udp_traf($i) [new Application/Traffic/CBR]
+	$ns duplex-link $udp_src_n($i) $bottleneck 50Mb 2ms DropTail
+	$ns attach-agent $udp_src_n($i) $sctp_src($i)
+	$udp_traf($i) attach-agent $udp_src($i) 
 	# sink
-	set udp-snk-n($i) [$ns node]
-	$ns duplex-link $bottleneck $udp-snk-n($i)  5Mb 20ms DropTail
-	$ns queue-limit $bottleneck $udp-snk-n($i) 300
-	set udp-src($i) [new Agent/Null]
-	$ns attach-agent $sctp-src-n($i) $sctp-src($i)
+	set udp_snk_n($i) [$ns node]
+	$ns duplex-link $bottleneck $udp_snk_n($i)  5Mb 20ms DropTail
+	$ns queue-limit $bottleneck $udp_snk_n($i) 300
+	set udp_src($i) [new Agent/Null]
+	$ns attach-agent $sctp_src_n($i) $sctp_src($i)
 
-	$ns connect udp-src($i) udp-snk($i)
+	$ns connect $udp_src($i) $udp_snk($i)
 }
